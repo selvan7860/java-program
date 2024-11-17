e21d5881d34b43cfb14524cf9d6838900e6bd900
selvan7860 <lakshmanaselvan252@gmail.com>
2024-11-17 11:22:27 +0530

    compare to

--- StringCodePointAt.java
+++ StringCodePointAt.java
@@ -2,6 +2,7 @@ public class StringCodePointAt{
     public static void main(String[] args) {
         CodePointAt();
         CodePointBefore();
+        StringCompareTo();
     }
 
     public static void CodePointAt(){
@@ -14,5 +15,11 @@ public class StringCodePointAt{
         String str = "Hello";
         int result = str.codePointBefore(2);
         System.out.println(result);
+    }
+
+    public static void StringCompareTo(){
+        String str1 = "Hello";
+        String str2 = "Hello New";
+        System.out.println(str1.compareTo(str2));
     }
 }
